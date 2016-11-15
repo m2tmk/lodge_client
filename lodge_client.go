e5ad6a953d5e2672daa2ac48171a78dcbe8af7c4
Tamaki Matsumoto <t.matsumoto@japantaxi.co.jp>
2016-11-15 21:00:31 +0900

    temp commit

--- lodge_client.go
+++ lodge_client.go
@@ -1,3 +1,24 @@
 package main
 
+import (
+  "log"
+  "net"
+  "time"
+  "strconv"
+)
 
+func main() {
+  serverAddr, err := net.ResolveUDPAddr("udp","localhost:54300")
+  fatalError(err)
+
+  conn, err := net.DialUDP("udp", nil, serverAddr)
+  fatalError(err)
+
+  defer conn.Close()
+}
+
+func fatalError(err error){
+  if err != nil {
+    log.Fatal("error: ", err.Error())
+  }
+}
