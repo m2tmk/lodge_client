de657faac30240034c2d7944fe5aeea8fe9a6eaa
Tamaki Matsumoto <t.matsumoto@japantaxi.co.jp>
2016-12-06 20:40:08 +0900

    client - server communication implemented

--- lodge_client.go
+++ lodge_client.go
@@ -8,13 +8,30 @@ import (
 )
 
 func main() {
-  serverAddr, err := net.ResolveUDPAddr("udp","localhost:54300")
+  serverAddr, err := net.ResolveUDPAddr("udp", "localhost:54321")
   fatalError(err)
 
-  conn, err := net.DialUDP("udp", nil, serverAddr)
+  clientAddr, err := net.ResolveUDPAddr("udp", "localhost:54300")
+  fatalError(err)
+
+  conn, err := net.DialUDP("udp", clientAddr, serverAddr)
   fatalError(err)
 
   defer conn.Close()
+
+  i := 0
+  for {
+    msg := strconv.Itoa(i)
+
+    i++
+    buffer := []byte(msg)
+    _, err := conn.Write(buffer)
+    fatalError(err)
+
+    log.Printf("Send: [%v]: %v\n", serverAddr, msg)
+
+    time.Sleep(time.Duration(300) * time.Millisecond)
+  }
 }
 
 func fatalError(err error){
